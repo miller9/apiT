45b6951f6842054c04c9f87d4cce932d45d9d869
miller9 <millerpoli9@gmail.com>
2019-04-07 20:59:57 -0500

    Select all articles in DB to return them instead of an empty json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,11 @@ class ArticlesController < ApplicationController
   def index
 
     # initially, this way, the http request test can pass
-    render json: {}
+    #render json: {}
+
+    # this way, I select all articles in DB and return them instead of an empty json
+    articles = Article.all
+    render json: articles
 
   end
 
